6561e798652e749998de9f1e1f6fe6c0e4f907c9
liuyl <1095816743@qq.com>
2020-12-01 11:44:22 +0800

    刘瑶力用idea的操作没有用命令第一次提交项目

---
+++ src/main/java/com/baizhi/Lyl2005ideaApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lyl2005ideaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lyl2005ideaApplication.class, args);
+    }
+
+}
